db3b9e27910fed9ad87d9a303e6942332300938f
malachi.obadeyi <malachi.obadeyi@sainsburys.co.uk>
2022-09-07 20:48:51 +0100

    remove unused modules

--- cmd/githubwebhookserver/main.go
+++ cmd/githubwebhookserver/main.go
@@ -31,7 +31,6 @@ import (
 	"github.com/actions-runner-controller/actions-runner-controller/github"
 	"github.com/actions-runner-controller/actions-runner-controller/logging"
 	"github.com/kelseyhightower/envconfig"
-	"go.uber.org/zap/zapcore"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -39,7 +38,6 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	// +kubebuilder:scaffold:imports
 )
 
